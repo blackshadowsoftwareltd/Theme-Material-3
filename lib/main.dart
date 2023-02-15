3fb8659340998ae365c69fbefcbc462b9e0f1ad8
remon <blackshadowsoftwareltd@gmail.com>
2023-02-15 22:51:10 +0600

    theme init

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Main(),
+  ));
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
