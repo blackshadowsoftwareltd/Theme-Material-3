7278ae2d27a31d6af012ce981ad49ce6e2c34fc7
remon <blackshadowsoftwareltd@gmail.com>
2023-02-17 00:45:25 +0600

    code refactor

--- lib/helpers/themes/light.dart
+++ lib/helpers/themes/light.dart
@@ -1,23 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:theme_material_3/helpers/colors/colors.dart';
+import 'color_schemes.dart' show lightColorScheme;
+import 'styles/appbar.dart' show lightAppBarTheme;
 
 ThemeData lightTheme = ThemeData(
   useMaterial3: true,
-  appBarTheme: appBarTheme,
+  appBarTheme: lightAppBarTheme,
   scaffoldBackgroundColor: white,
   visualDensity: VisualDensity.adaptivePlatformDensity,
   fontFamily: GoogleFonts.baloo2().fontFamily,
-);
-
-/// AppBar
-AppBarTheme appBarTheme = AppBarTheme(
-  backgroundColor: white,
-  foregroundColor: grey800,
-  systemOverlayStyle: SystemUiOverlayStyle.light,
-  shadowColor: black,
-  elevation: 5,
-  shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(bottom: Radius.elliptical(200, 10))),
+  colorScheme: lightColorScheme,
 );
