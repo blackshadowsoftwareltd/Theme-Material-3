92b4b8e837f906585d9bed3906afba0fe5121975
remon <blackshadowsoftwareltd@gmail.com>
2023-02-18 12:42:03 +0600

    elevated buttons

--- lib/modules/buttons/components/elevated.dart
+++ lib/modules/buttons/components/elevated.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+
 class ElevatedButtons extends StatelessWidget {
   const ElevatedButtons({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        const Text('Elevated Buttons'),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(onPressed: () {}, child: const Text('Elevated')),
+            ElevatedButton.icon(
+              onPressed: () {},
+              label: const Text('Add'),
+              icon: const Icon(Icons.add),
+            ),
+          ],
+        ),
+        const Divider(),
+      ],
+    );
   }
 }
